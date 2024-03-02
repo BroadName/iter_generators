4f59fa26368a4fbce77c73ec7c28077adb6094b6
BroadName <flintbistvud@mail.ru>
2024-03-02 17:48:57 +0600

    initial commit

---
+++ task_4.py
@@ -0,0 +1,41 @@
+import types
+
+
+def flat_generator(list_of_lists):
+    temp = list_of_lists
+    c = []
+    check = True
+    while check:
+        c = []
+        check = False
+        for j in temp:
+            if isinstance(j, list):
+                check = True
+                c.extend(j)
+            else:
+                c.append(j)
+
+        temp = c
+    yield from c
+
+
+def test_4():
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+    assert isinstance(flat_generator(list_of_lists_2), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_4()
